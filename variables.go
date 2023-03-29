067a6c578fb9487c74f128185ec30e3f90ccedf5
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-29 09:54:50 +0200

    Variable definitions in go

---
+++ variables.go
@@ -0,0 +1,10 @@
+// Variable definitions
+
+var name string = "Kwaleyela"
+var gender string = "Male"
+var age int = 21
+
+// Variable automatically knows type
+name := "Kwaleyela"
+gender := "Male"
+age := 21
